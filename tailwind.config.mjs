d8277a01cc044196cf7e16e00c95566a662a89c5
Chris Pennington <chris@codinginpublic.dev>
2024-03-15 12:31:52 -0600

    feat(app): add marquee mvp

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -35,6 +35,20 @@ export default {
         sans: ["DM Sans Variable", ...defaultTheme.fontFamily.sans],
         mono: ["DM Mono", ...defaultTheme.fontFamily.mono],
       },
+      animation: {
+        marquee: "marquee 40s linear infinite",
+        marquee2: "marquee2 40s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
     },
   },
   plugins: [],
