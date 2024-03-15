21038d8d194c577210828af8e91f1ca021ffa8e0
Chris Pennington <chris@codinginpublic.dev>
2024-03-15 14:39:03 -0600

    feat(app): mvp footer

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -7,3 +7,21 @@ export const keyLinks = {
   github: "https://github.com/Share-Brewfiles",
   npm: "https://www.npmjs.com/package/share-brewfiles",
 };
+
+export const contributors = [
+  {
+    name: "Jess Wang",
+    role: "Dev Rel",
+    href: "https://twitter.com/daRubberDuckiee",
+  },
+  {
+    name: "Chris Pennington",
+    role: "Dev",
+    href: "https://twitter.com/cpenned",
+  },
+  {
+    name: "Kyle Ribant",
+    role: "Design",
+    href: "/",
+  },
+];
