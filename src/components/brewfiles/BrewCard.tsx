81507057115f5030c1b77dcd1b012db852dcb302
Chris Pennington <chris@codinginpublic.dev>
2024-05-07 12:46:21 -0600

    change links in cards

--- src/components/brewfiles/BrewCard.tsx
+++ src/components/brewfiles/BrewCard.tsx
@@ -16,24 +16,34 @@ const BrewCard = ({ entry }: Props) => {
       }`}
     >
       <div className="p-4 @sm:p-6 flex flex-col gap-4 @sm:gap-6 justify-between">
-        <a
-          className="flex items-center gap-3 group rounded-full focus:outline-none focus-visible:ring-4 ring-offset-4 ring-offset-bkg ring-accent-bluemedium"
-          href={entry ? `https://github.com/${entry.username}` : "#"}
-        >
-          <img
-            src={
-              entry ? `https://github.com/${entry.username}.png` : anonImage.src
-            }
-            alt="anon"
-            width="50"
-            height="50"
-            className="rounded-full grayscale group-hover:ring-4 "
-          />
-          <p className="font-mono @sm:text-lg @md:text-xl @lg:text-2xl uppercase tracking-widest group-hover:underline underline-offset-4 decoration-white/20 break-all">
+        <div className="flex justify-between items-center gap-2 @sm:gap-3">
+          <a
+            className="flex items-center gap-3 rounded-full focus:outline-none focus-visible:ring-4 ring-offset-4 ring-offset-bkg ring-accent-bluemedium"
+            href={entry ? `https://github.com/${entry.username}` : "#"}
+          >
+            <img
+              src={
+                entry
+                  ? `https://github.com/${entry.username}.png`
+                  : anonImage.src
+              }
+              alt="anon"
+              width="50"
+              height="50"
+              className="rounded-full grayscale hover:ring-4 "
+            />
+          </a>
+          <a
+            className="font-mono @sm:text-lg @lg:text-xl @2xl:text-2xl uppercase tracking-widest hover:underline underline-offset-4 decoration-white/20 break-all flex items-center gap-3 group rounded-full focus:outline-none focus-visible:ring-4 ring-offset-4 ring-offset-bkg ring-accent-bluemedium"
+            href={entry && `/brew/${entry.id}`}
+          >
             {entry ? entry.username : "Username"}
-          </p>
-          {entry?.isFeatured && <img src={starIcon.src} alt="star" />}
-        </a>
+          </a>
+
+          {entry?.isFeatured && (
+            <img src={starIcon.src} alt="star" width={20} />
+          )}
+        </div>
 
         <div className="grid gap-1">
           <p className="@md:text-lg uppercase tracking-widest text-accent-bluelight min-h-5 @md:min-h-7">
