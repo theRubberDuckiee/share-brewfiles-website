3c1ced70cd020188b9d9abd01c7672ba706b8b76
Jess <jess@warp.dev>
2024-05-30 19:40:44 -0700

    add auth for cli

---
+++ src/pages/api/exchangeCodeForAccessToken.ts
@@ -0,0 +1,53 @@
+import type { APIRoute } from "astro";
+
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const { code } = await request.json();
+    if (!code) {
+      throw new Error("No code was given!");
+    }
+
+    const response = await fetch(`https://github.com/login/oauth/access_token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        client_id: "6db8c19033a30994dffa",
+        client_secret: GITHUB_CLIENT_SECRET,
+        code: code
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        "We failed to fetch your user information from the GitHub API."
+      );
+    }
+    
+    return new Response(
+        JSON.stringify({ success: true, access_token: response.data.access_token, updated: true }),
+        {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: error instanceof Error ? error.message : error,
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+};
