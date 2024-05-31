766cb3504c92100b9a718c9d58452940db04609c
Jess <jess@warp.dev>
2024-05-30 19:54:01 -0700

    fixing api endpoint

--- src/pages/api/exchangeCodeForAccessToken.ts
+++ src/pages/api/exchangeCodeForAccessToken.ts
@@ -8,28 +8,29 @@ export const POST: APIRoute = async ({ request }) => {
     if (!code) {
       throw new Error("No code was given!");
     }
-
+    
     const response = await fetch(`https://github.com/login/oauth/access_token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        client_id: "6db8c19033a30994dffa",
-        client_secret: GITHUB_CLIENT_SECRET,
-        code: code
-      })
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        "We failed to fetch your user information from the GitHub API."
-      );
-    }
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          client_id: "6db8c19033a30994dffa",
+          client_secret: GITHUB_CLIENT_SECRET,
+          code: code
+        })
+      });
+      
+      if (!response.ok) {
+        throw new Error("We failed to fetch your user information from the GitHub API.");
+      }
+      
+    const responseData = await response.json();
+    const accessToken = responseData.access_token;
     
     return new Response(
-        JSON.stringify({ success: true, access_token: response.data.access_token, updated: true }),
+        JSON.stringify({ success: true, access_token: accessToken, updated: true }),
         {
           status: 200,
           headers: {
